70db68dec97bdf8bcf75d134145a4735d658c823
Natasha Singh <dukedesi22@gmail.com>
2019-10-14 16:53:42 -0400

    :package: Update py package and config with fhir version R4

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import os
 from setuptools import setup, find_packages
 
+from kf_model_fhir.config import FHIR_VERSION
+
 root_dir = os.path.dirname(os.path.abspath(__file__))
 req_file = os.path.join(root_dir, 'requirements.txt')
 with open(req_file) as f:
     requirements = f.read().splitlines()
 
-fhir_version = __import__('kf_model_fhir').__fhir_version__
 version = __import__('kf_model_fhir').__version__
 
 setup(
     name='kf-model-fhir',
     version=version,
-    description=f'Kids First FHIR Model {fhir_version}',
+    description=f'Kids First FHIR Model {FHIR_VERSION}',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
