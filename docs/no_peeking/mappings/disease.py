29b8490df3cf974682943573bb3766e8191f206c
liberaliscomputing <liberalis.computing@gmail.com>
2020-02-28 09:24:54 -0500

    :construction: functify disease

--- docs/no_peeking/mappings/disease.py
+++ docs/no_peeking/mappings/disease.py
@@ -1,85 +1,83 @@
 """
 This module maps Kids First diagnosis to Phenopackets Disease (derived from FHIR Condition).
 Please visit https://aehrc.github.io/fhir-phenopackets-ig/StructureDefinition-Disease.html
-    for the detailed structure definition.
+for the detailed structure definition.
 """
 from kf_lib_data_ingest.common import constants
 from kf_lib_data_ingest.common.concept_schema import CONCEPT
 
+from .individual import resource_type as iRType
+from .shared import get, codeable_concept, make_identifier, make_select, GO_AWAY_SERVER
 
-def coded_onset(x):
-    """
-    https://aehrc.github.io/fhir-phenopackets-ig/ValueSet-Onset.html
-    """
-    pass
-
-def tumor_stage(x):
-    """
-    https://aehrc.github.io/fhir-phenopackets-ig/ValueSet-TumorStage.html
-    """
-    pass
-
-def disease_code(x):
-    """
-    http://hl7.org/fhir/R4/valueset-condition-code.html
-    """
-    pass
-
-def body_site(x):
-    """
-    http://hl7.org/fhir/R4/valueset-body-site.html
-    """
-    pass
+resource_type = "Condition"
 
 
-disease =  {
-    'resourceType': 'Condition',
-    'id': CONCEPT.DIAGNOSIS.TARGET_SERVICE_ID,
-    'meta': {
-        'profile': ['http://ga4gh.fhir.phenopackets/StructureDefinition/Disease']
-    },
-    'extension': [
-        {
-            'text': 'CodedOnset',
-            'url': 'http://ga4gh.org/fhir/phenopackets/StructureDefinition/CodedOnset',
-            'valueCodeableConcept': {
-                'coding': [
-                    {
-                        'system': 'http://purl.obolibrary.org/obo/hp.owl',
-                        'code': 'HP:0410280',
-                        'display': 'Pediatric onset'
-                    }
-                ]
-            }
-        }
-    ],
-    'identifier': [
-        {
-            'system': 'https://kf-api-dataservice.kidsfirstdrc.org/diagnoses',
-            'value': CONCEPT.DIAGNOSIS.TARGET_SERVICE_ID
-        },
-        {
-            'value': CONCEPT.DIAGNOSIS.ID
-        }
-    ],
-    'code': {
-        'coding': None,	# condition_code()
-        'text':	CONCEPT.DIAGNOSIS.NAME
-    },
-    'bodySite': None, # body_site()
-    'subject': {
-        'reference': f'Patient/{CONCEPT.PARTICIPANT.TARGET_SERVICE_ID}',
-        'type': 'Patient',
-        'identifier': [
-            {
-                'system': 'https://kf-api-dataservice.kidsfirstdrc.org/participants',
-                'value': CONCEPT.PARTICIPANT.TARGET_SERVICE_ID
+def yield_diseases(eng, table, study_id, individuals):
+    for row in make_select(
+            eng, table,
+            CONCEPT.DIAGNOSIS.ID,
+            CONCEPT.DIAGNOSIS.TARGET_SERVICE_ID,
+            CONCEPT.PARTICIPANT.ID,
+            CONCEPT.DIAGNOSIS.NAME,
+            CONCEPT.DIAGNOSIS.EVENT_AGE_DAYS
+        ):
+        id = get(row, CONCEPT.DIAGNOSIS.ID)
+        kfid = get(row, CONCEPT.DIAGNOSIS.TARGET_SERVICE_ID)
+        subject_id = get(row, CONCEPT.PARTICIPANT.ID)
+        diagnosis_name = get(row, CONCEPT.DIAGNOSIS.NAME)
+        event_age_days = get(row, CONCEPT.DIAGNOSIS.EVENT_AGE_DAYS)
+
+        if not id:
+            continue
+
+        retval = {
+            "resourceType": resource_type,
+            "text": {
+                "status": "empty",
+                "div": GO_AWAY_SERVER
             },
-            {
-                'value': CONCEPT.PARTICIPANT.ID
+            "id": make_identifier(resource_type, study_id, id),
+            "meta": {
+                "profile": ["http://ga4gh.org/fhir/phenopackets/StructureDefinition/Disease"]
+            },
+            "identifier": [
+                {
+                    "system": f"http://kf-api-dataservice.kidsfirstdrc.org/diagnoses?study_id={study_id}", "value": id
+                }
+            ]
+        }
+
+        if kfid:
+            retval["identifier"].append(
+                {
+                    "system": "http://kf-api-dataservice.kidsfirstdrc.org/diagnoses", "value": kfid
+                }
+            )
+        
+        if subject_id:
+            retval["subject"] = {
+                "reference": f"Patient/{individuals[subject_id]['id']}",
+                "type": iRType
             }
-        ],
-        'display': CONCEPT.PARTICIPANT.ID
-    },
-    'onsetAge': CONCEPT.DIAGNOSIS.EVENT_AGE_DAYS
-}
+
+        if diagnosis_name:
+            retval.setdefault("extension", []).append(
+                {
+                    "url": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/CodedOnset",
+                    "valueCodeableConcept": {
+                        "coding": [
+                            {
+                                "system": "http://purl.obolibrary.org/obo/hp.owl",
+                                "code": "HP:0410280",
+                                "display": "Pediatric onset"
+                            }
+                        ]
+                    }
+                }
+            )
+            retval.setdefault("code", {})["text"] = diagnosis_name
+        
+        if event_age_days:
+            retval['onsetAge'] = f'{event_age_days}d'
+
+        yield retval, id
