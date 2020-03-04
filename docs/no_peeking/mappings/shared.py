90c936ed34515407def545c7a2dec2ae501fd373
liberaliscomputing <liberalis.computing@gmail.com>
2020-03-04 10:18:02 -0500

    :construction: add id max char restriction

--- docs/no_peeking/mappings/shared.py
+++ docs/no_peeking/mappings/shared.py
@@ -21,12 +21,13 @@ def codeable_concept(x, systems, name):
     }
 
 
+# http://hl7.org/fhir/R4/datatypes.html#id
 def make_identifier(*args):
     return re.sub(
         r"[^A-Za-z0-9\-\.]",
         "-",
         ".".join(str(a) for a in args)
-    )
+    )[:64]
 
 
 def make_select(eng, table, *args):
