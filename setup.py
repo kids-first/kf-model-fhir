81a83d7f61b2bc4e99794739f7b6926d5170cd92
Natasha Singh <dukedesi22@gmail.com>
2020-05-05 16:29:18 -0400

    :recycle: Rm CLI entrypoint from setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import os
 from setuptools import setup, find_packages
 
-from kf_model_fhir.config import FHIR_VERSION
-
 root_dir = os.path.dirname(os.path.abspath(__file__))
 req_file = os.path.join(root_dir, 'requirements.txt')
 with open(req_file) as f:
     requirements = f.read().splitlines()
 
+FHIR_VERSION = "4.0.1"
 version = __import__('kf_model_fhir').__version__
 
 setup(
@@ -15,11 +14,6 @@ setup(
     version=version,
     description=f'Kids First FHIR Model {FHIR_VERSION}',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'fhirmodel=kf_model_fhir.cli:cli',
-        ],
-    },
     include_package_data=True,
     install_requires=requirements
 )
