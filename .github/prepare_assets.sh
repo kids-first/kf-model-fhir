59cb8c6f2e9ebd8c86f3cb9d24e9f4c5989dfeec
Avi Kelman <patcherton.fixesthings@gmail.com>
2020-07-10 17:33:02 -0600

    :bug::construction_worker: Get IG publishing to work
    Apparently github has special file system protection rules inside workflows.

--- .github/prepare_assets.sh
+++ .github/prepare_assets.sh
@@ -11,12 +11,14 @@ git remote set-branches origin '*'
 git fetch origin gh-pages
 git checkout -f gh-pages
 git rm -rf --ignore-unmatch ig
-mv -f site_root/output ig
-mkdir -p not_needed_for_ig_site
-mv ig/*.zip not_needed_for_ig_site
-mv ig/*.tgz not_needed_for_ig_site
-mv ig/*.pack not_needed_for_ig_site
+sudo mv -f site_root/output ig
+sudo mkdir -p not_needed_for_ig_site
+sudo mv ig/*.zip not_needed_for_ig_site
+sudo mv ig/*.tgz not_needed_for_ig_site
+sudo mv ig/*.pack not_needed_for_ig_site
 git add ig
+git config --global user.email "no_email"
+git config --global user.name "gh_action_bot"
 git commit -m "Update IG for release ${VERSION}"
 if [ $? -ne 0 ]; then
     echo "nothing to commit"
